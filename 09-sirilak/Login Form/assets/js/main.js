2767ab1049b181f0698f5748d8207420de2ad58a
NSIRI-K <snut2256@gmail.com>
2025-11-24 16:21:44 +0700

    si

--- 09-sirilak/Login Form/assets/js/main.js
+++ 09-sirilak/Login Form/assets/js/main.js
@@ -1,4 +1,16 @@
 /*=============== HIDE & SHOW PASSWORD ===============*/
+const showHiddenPass = (password, eye) => {
+    const input = document.getElementById(password),
+          iconEye = document.getElementById(eye)
 
+    iconEye.addEventListener('click', () => {
+        input.type === 'password' ? input.type = 'text'
+                                  :  input.type = 'password'
+
+        iconEye.classList.toString('ri-eye-off-line')
+        iconEye.classList.toggle('ri-eye-off-line')
+    })
+}
+showHiddenPass('loginPass','loginEye')
 
 /*=============== SWIPER IMAGES ===============*/
