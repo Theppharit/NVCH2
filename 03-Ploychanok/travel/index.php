75e031371c50ed0f9a375b2353a9cb537ec2fdf8
VakerikPhattharin <bossgg2551@gmail.com>
2025-12-08 15:07:15 +0700

    .

---
+++ 03-Ploychanok/travel/index.php
@@ -0,0 +1,108 @@
+<?php include('include/head.php') ?>
+
+    
+    
+
+<body>
+
+<?php include('include/navbar.php') ?>
+
+    <main class="main">
+            <!--==================== HOME ====================-->
+            <section class="home" id="home">
+                <img src="assets/img/home1-2.jpg" alt="" class="home__img">
+
+                <div class="home__container container grid">
+                    <div class="home__data">
+                        <span class="home__data-subtitle">Recommend Tourrist Attraction</span>
+                        <h1 class="home__data-title">Welcome <br> To <b>Nan <br></b></h1>
+                        <a href="#" class="button">Explore</a>
+
+                    </div>
+
+                    <div class="home__social">
+                        <a href="https://www.facebook.com/" target="_blank" class="home__social-link">
+                            <i class="ri-facebook-box-fill"></i>
+                        </a>
+                        <a href="https://www.instagram.com/" target="_blank" class="home__social-link">
+                            <i class="ri-instagram-fill"></i>
+                        </a>
+                        <a href="https://twitter.com/" target="_blank" class="home__social-link">
+                            <i class="ri-twitter-fill"></i>
+                        </a>
+                    </div>
+
+                    <div class="home__info">
+                        <div>
+                            <span class="home__info-title">5 best places to visit</span>
+                            <a href="" class="button button--flex button--link home__info-button">
+                                More <i class="ri-arrow-right-line"></i>
+                            </a>
+                        </div>
+
+                        <div class="home__info-overlay">
+                            <img src="assets/img/home2-2.jpg" alt="" class="home__info-img">
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!--==================== ABOUT ====================-->
+
+            <!--==================== DISCOVER ====================-->
+
+            <!--==================== EXPERIENCE ====================-->
+            <section class="experience section">
+                <h2 class="section__title">With Our Experience <br> We Will Recommend You</h2>
+
+                <div class="experience__container container grid">
+                    <div class="experience__content grid">
+                        
+                    </div>
+
+                    <div class="experience__img grid">
+                        <div class="experience__overlay">
+                            <img src="assets/img/experience1-2.jpg" alt="" class="experience__img-one">
+                        </div>
+                        
+                        <div class="experience__overlay">
+                            <img src="assets/img/experience2-2.jpg" alt="" class="experience__img-two">
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!--==================== VIDEO ====================-->
+            <section class="video section">
+                <h2 class="section__title">Video Tour</h2>
+
+                <div class="video__container container">
+                    <p class="video__description">Find out more with our video of the most beautiful and 
+                        pleasant places for you and your family.
+                    </p>
+
+                    <div class="video__content">
+                        <video id="video-file">
+                            <source src="assets/video/video2.mp4" type="video/mp4">
+                        </video>
+
+                        <button class="button button--flex video__button" id="video-button">
+                            <i class="ri-play-line video__button-icon" id="video-icon"></i>
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+            <!--==================== PLACES ====================-->
+
+            <!--==================== SUBSCRIBE ====================-->
+           
+            
+            <!--==================== SPONSORS ====================-->
+           
+        </main>
+</body>
+<?php include('include/footer.php') ?>
+
+<?php include('include/addjs.php') ?>
+
---
+++ 03-Ploychanok/travel/places.php
@@ -0,0 +1,123 @@
+<?php include('include/head.php') ?>
+<body>
+<?php include('include/navbar.php') ?>
+<section class="place section" id="place">
+
+
+    
+                <h2 class="section__title">Choose Your Place</h2>
+
+                <div class="place__container container grid">
+                    <!--==================== PLACES CARD 1 ====================-->
+                    <div class="place__card">
+                        <img src="assets/img/place1-2.jpg" alt="" class="place__img">
+                        
+                        <div class="place__content">
+                            <span class="place__rating">
+                                <i class="ri-star-line place__rating-icon"></i>
+                                <span class="place__rating-number">4.7</span>
+                            </span>
+
+                            <div class="place__data">
+                                <h3 class="place__title">วัดภูมินทร์</h3>
+                                <span class="place__subtitle">ถนนผากอง บ้านภูมินทร์ ตำบลในเวียง อำเภอเมืองน่าน จังหวัดน่าน</span>
+                             
+                            </div>
+                        </div>
+
+                        <button class="button button--flex place__button">
+                            <i class="ri-arrow-right-line"></i>
+                        </button>
+                    </div>
+
+                    <!--==================== PLACES CARD 2 ====================-->
+                    <div class="place__card">
+                        <img src="assets/img/place2-2.jpg" alt="" class="place__img">
+                        
+                        <div class="place__content">
+                            <span class="place__rating">
+                                <i class="ri-star-line place__rating-icon"></i>
+                                <span class="place__rating-number">4.6</span>
+                            </span>
+
+                            <div class="place__data">
+                                <h3 class="place__title">วัดพระธาตุเขาน้อย</h3>
+                                <span class="place__subtitle">ตำบลดู่ใต้ อำเภอเมืองน่าน จังหวัดน่าน</span>
+                             
+                            </div>
+                        </div>
+
+                        <button class="button button--flex place__button">
+                            <i class="ri-arrow-right-line"></i>
+                        </button>
+                    </div>
+
+                    <!--==================== PLACES CARD 3 ====================-->
+                    <div class="place__card">
+                        <img src="assets/img/place3-2.jpg" alt="" class="place__img">
+                        
+                        <div class="place__content">
+                            <span class="place__rating">
+                                <i class="ri-star-line place__rating-icon"></i>
+                                <span class="place__rating-number">4.5</span>
+                            </span>
+
+                            <div class="place__data">
+                                <h3 class="place__title">ดอยเสมอดาว</h3>
+                                <span class="place__subtitle">ตำบลศรีษะเกษ อำเภอนาน้อย จังหวัดน่าน</span>
+                            
+                            </div>
+                        </div>
+
+                        <button class="button button--flex place__button">
+                            <i class="ri-arrow-right-line"></i>
+                        </button>
+                    </div>
+
+                    <!--==================== PLACES CARD 4 ====================-->
+                    <div class="place__card">
+                        <img src="assets/img/place4-2.jpg" alt="" class="place__img">
+                        
+                        <div class="place__content">
+                            <span class="place__rating">
+                                <i class="ri-star-line place__rating-icon"></i>
+                                <span class="place__rating-number">4.5</span>
+                            </span>
+
+                            <div class="place__data">
+                                <h3 class="place__title">หอศิลป์ริมน่าน</h3>
+                                <span class="place__subtitle">ตำบลบ่อ อำเภอเมืองน่าน จังหวัดน่าน</span>
+                               
+                            </div>
+                        </div>
+
+                        <button class="button button--flex place__button">
+                            <i class="ri-arrow-right-line"></i>
+                        </button>
+                    </div>
+
+                    <!--==================== PLACES CARD 5 ====================-->
+                    <div class="place__card">
+                        <img src="assets/img/place5-2.jpg" alt="" class="place__img">
+                        
+                        <div class="place__content">
+                            <span class="place__rating">
+                                <i class="ri-star-line place__rating-icon"></i>
+                                <span class="place__rating-number">4.2</span>
+                            </span>
+
+                            <div class="place__data">
+                                <h3 class="place__title">อุโมงค์ลีลาวดี</h3>
+                                <span class="place__subtitle">ตำบลในเวียง​ อำเภอเมืองน่าน จังหวัดน่าน </span>
+                               
+                            </div>
+                        </div>
+
+                        <button class="button button--flex place__button">
+                            <i class="ri-arrow-right-line"></i>
+                        </button>
+                    </div>
+                </div>
+            </section>
+</body>
+<?php include('include/footer.php') ?> 
