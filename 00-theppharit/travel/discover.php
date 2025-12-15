a0bf9c31556752b2fc1eab0323f75930f494bef9
Th.To. <theppharit7@gmail.com>
2025-12-15 13:38:00 +0700

    +

--- 00-theppharit/travel/discover.php
+++ 00-theppharit/travel/discover.php
@@ -4,9 +4,9 @@
 
     <?php include('include/navbar.php') ?>
 
-    <main class="main">
+    <main class="main" id="discover">
         <!--==================== DISCOVER ====================-->
-        <section class="discover section" id="discover">
+        <section class="discover section">
             <h2 class="section__title">Discover the most <br> attractive places</h2>
 
             <div class="discover__container container swiper-container">
@@ -108,7 +108,10 @@
 
     <?php include('include/footer.php') ?>
 
+    <?php include('include/add-video.php') ?>
+
     <?php include('include/add-js.php') ?>
+
 
 </body>
 
